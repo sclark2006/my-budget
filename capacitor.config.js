7c1ee302f95f5e4138dde06bc975a9df36ce50f3
Frederick Clark <sclark2006@gmail.com>
2022-04-15 11:33:25 -0400

    feat: Created the blank Ionic app and removed ts

---
+++ capacitor.config.js
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'my-budget',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
